97d751a2cea14bab31f3e1dec467b27d57b619a2
talhajjaj <taima.hajaj@gmail.com>
2021-06-28 16:06:18 +0300

    solving lab2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import Main from './components/Main';
 import HornedBeasts from './components/HornedBeasts';
 import Footer from './components/Footer';
-
+import './App.css';
 
 class App extends React.Component{
   render(){
--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,11 +1,36 @@
 import React from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card'
 class HornedBeasts extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            numberOfLikes:0
+        }
+    }
+
+    increaseLikes=() =>{
+        this.setState({
+            numberOfLikes:this.state.numberOfLikes +1
+
+        })
+    }
     render(){
         return(
-            <div className ='HornedBeasts'>HornedBeastsName{this.props.HornedBeastsName}</div>
-      
-            )
-    }
+        
+            <Card style={{ width: '18rem' }}>
+            <Card.Img onClick={this.increaseLikes} variant="top" src={this.props.image_url} />
+            <Card.Body>
+                <Card.Title>{this.props.title}☘️</Card.Title>
+                <Card.Text>
+                press on your favorite beast 🦄🦄
+                </Card.Text>
+                <Card.Text>
+                Likes : 💖 {this.state.numberOfLikes} 💖
+                </Card.Text>
+                
+            </Card.Body>
+        </Card>
+        )}
 }
 export default HornedBeasts;
